8c772158d5fd32a1aaf45bae0a5c546abaf3cce7
Gabriel D. <gabrieldahan05@gmail.com>
2024-04-07 18:22:05 +0200

    URL parsing

--- core.py
+++ core.py
@@ -1,11 +1,34 @@
 import requests
 from datetime import datetime
+from collections import namedtuple
+
+try:
+    # Python 3
+    from urllib.parse import urlunparse, urlencode
+except ImportError:
+    # Python 2
+    print('Try upgrading to Python3.')
+    exit(1)
 
 class RATPAPI:
     
     def __init__(self) -> None:
-        self.base_api = 'https://api-iv.iledefrance-mobilites.fr/lines/line:IDFM:{0}/schedules?it=true&&complete={1}&date={2}'
-        self.base_stations = 'https://api-iv.iledefrance-mobilites.fr/lines/line:IDFM:{0}/stops?stopPoints=false&routes=false'
+
+        self.components = namedtuple(
+            typename = 'base_components',
+            field_names = ['scheme', 'netloc', 'url', 'params', 'query', 'fragment']
+        )
+
+        self.base: str = urlunparse(
+            self.components(
+                scheme = 'https',
+                netloc = 'api-iv.iledefrance-mobilites.fr',
+                url = '/lines/line:IDFM:{0}/{1}',
+                params = '',
+                query = '{2}',
+                fragment = ''
+            )
+        )
         self.base_times = 'https://api-iv.iledefrance-mobilites.fr/lines/v2/line:IDFM:{0}/stops/stop_area:IDFM:{1}/realTime'
         
         self.categ = [
@@ -36,25 +59,27 @@ class RATPAPI:
             'FUN': 'C01385'
         }
     
+    def __get_url(self, endpoint: str, lineid: str, params: dict) -> str:
+        return self.base.format(lineid, endpoint, urlencode(params))
+
     def __get_formatted_time(self) -> str:
         return datetime.now().strftime('%Y-%m-%d')
     
-    def get_line_info(self, line: str, complete: bool = False) -> dict:
-        res = requests.get(
-            self.base_api.format(
-                self.lines[line], 
-                str(complete).lower(),
-                self.__get_formatted_time()
-            )
-        )
+    def get_line_info(self, line: str, it: bool = True, complete: bool = False) -> dict:
+        params = {
+            'it': it,
+            'complete': complete,
+            'date': self.__get_formatted_time()
+        }
+        res = requests.get(self.__get_url('schedules', self.lines[line], params))
         return res.json()
     
-    def get_stations(self, line: str) -> dict:
-        res = requests.get(
-            self.base_stations.format(
-                self.lines[line]
-            )
-        )
+    def get_stations(self, line: str, stop_points: bool = False, routes: bool = False) -> dict:
+        params = {
+            'stopPoints': stop_points,
+            'routes' : routes
+        }
+        res = requests.get(self.__get_url('stops', self.lines[line], params))
         return res.json()
     
 if __name__ == '__main__':
